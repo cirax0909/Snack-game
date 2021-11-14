01813696af4f4deb5cc8b672b6b24b469955dbbb
cirax0909 <62638678+cirax0909@users.noreply.github.com>
2021-11-13 19:29:57 -0500

    add directional way of turtle 2, 3. They would follow the turtle 1.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import time
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snack Game")
+
+
+segments = []
+
 
 for n in range(0, 3):
     space = -20
@@ -12,5 +17,16 @@ for n in range(0, 3):
     tim.shape("square")
     tim.penup()
     tim.goto(x=0+n*space, y=0)
+    segments.append(tim)
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 screen.exitonclick()
